b86b5f60e43d889b007535f5f64109af479a2c78
Sat Naing Tun <satnaingtun.snt@gmail.com>
2023-11-18 07:57:57 +0630

    binding Test Form

---
+++ SNT_POS_Single_Sale/Test.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using SNT_POS_Common.Entity;
+
+namespace SNT_POS_Single_Sale
+{
+    public partial class Test : Form
+    {
+        List<Vouncher_Item> vouncher = new List<Vouncher_Item>();
+        
+        public Test()
+        {
+            InitializeComponent();
+        }
+
+        private void Test_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = vouncher;
+            dataGridView1.DataMember = "Vouncher_Item";
+        }
+    }
+}
